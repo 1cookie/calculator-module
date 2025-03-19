ae2c03c42b384cf7e3a270c897a0ff7cb6868d97
Andy C <andy.cookson@nexusrental.co.uk>
2025-03-19 07:45:45 +0000

    extend BaseCalculator with type VanDeliveryCalculator and instantiate its parent

--- app/Modules/Calculator/Services/VanDeliveryCalculator.php
+++ app/Modules/Calculator/Services/VanDeliveryCalculator.php
@@ -1,1 +1,11 @@
 <?php
+
+namespace App\Modules\Calculator\Services;
+
+class VanDeliveryCalculator extends BaseCalculator
+{
+    public function __construct()
+    {
+        parent::__construct(1.00, 15.00);
+    }
+}
