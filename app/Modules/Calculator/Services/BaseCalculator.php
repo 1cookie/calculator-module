8b50483131dc226e90bc504ddd4dec683fd62416
Andy C <andy.cookson@nexusrental.co.uk>
2025-03-19 07:42:57 +0000

    BaseCalculator implements calculate() method and returns array of; price, distance & drop off count etc...

--- app/Modules/Calculator/Services/BaseCalculator.php
+++ app/Modules/Calculator/Services/BaseCalculator.php
@@ -1,1 +1,33 @@
 <?php
+
+namespace App\Modules\Calculator\Services;
+
+use App\Modules\Calculator\Contracts\CalculatorInterface;
+
+abstract class BaseCalculator implements CalculatorInterface
+{
+    protected float $costPerMile;
+    protected float $extraPersonCost;
+
+    public function __construct(float $costPerMile, float $extraPersonCost)
+    {
+        $this->costPerMile = $costPerMile;
+        $this->extraPersonCost = $extraPersonCost;
+    }
+
+    public function calculate(array $distances, bool $extraPerson): array
+    {
+        $totalDistance = array_sum($distances);
+        $baseCost = $totalDistance * $this->costPerMile;
+        $extraCost = $extraPerson ? $this->extraPersonCost : 0;
+        $totalPrice = $baseCost + $extraCost;
+
+        return [
+            'drop_off_count' => count($distances),
+            'total_distance' => $totalDistance,
+            'cost_per_mile' => $this->costPerMile,
+            'extra_person_price' => $extraCost,
+            'total_price' => $totalPrice
+        ];
+    }
+}
